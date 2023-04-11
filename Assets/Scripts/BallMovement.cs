4e80bfe472462f4e3a2b8d2ffbcce8d2c7ba4a37
Ashish Verma <ashv6561@gmail.com>
2023-04-11 12:44:44 +0530

    Added Direction Change on Square Brick.

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] Transform BALL_HOLDER;
+    [SerializeField] float X_BOUND;
+    [SerializeField] Vector2 direction;
+    [SerializeField] bool isTurnComplete;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetBall();
+    }
+
+    void ResetBall() {
+        isTurnComplete = true;
+        transform.position = BALL_HOLDER.position;
+    }
+
+    private void FixedUpdate() {
+        direction = direction.normalized;
+        Vector3 pos = transform.position;
+        pos.y += direction.y * speed * Time.fixedDeltaTime;
+        if ((isTurnComplete && Mathf.Abs(pos.x + direction.x * speed * Time.fixedDeltaTime) < X_BOUND) || !isTurnComplete) {
+            pos.x += direction.x * speed * Time.fixedDeltaTime;    
+        }
+        transform.position = pos;
+        if (isTurnComplete)
+            BALL_HOLDER.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTurnComplete) {
+            if (Input.GetKey(KeyCode.A)) {
+                direction = Vector2.left;
+            } else if (Input.GetKey(KeyCode.D)) {
+                direction = Vector2.right;
+            } else {
+                direction = Vector2.zero;
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Tag : " + other.gameObject.tag);
+        if (other.gameObject.CompareTag("Top-Wall")) {
+            direction.y = -direction.y;
+        } else if (other.gameObject.CompareTag("Vertical-Wall")) {
+            direction.x = -direction.x;
+        } else if (other.gameObject.CompareTag("Square-Brick")) {
+            // HANDLE SQUARE BRICK
+            HandleSquareBrickCollision(other.gameObject);
+        } else if (other.gameObject.CompareTag("Circle-Brick")) {
+            // HANDLE CIRCULAR BRICK
+            HandleCircularBrickCollision(other.gameObject);
+        } else if (other.gameObject.CompareTag("Ground")) {
+            // HANDLE GROUND
+            StartCoroutine(BackToHolder());
+        }
+    }
+
+    void HandleCircularBrickCollision(GameObject brick) {
+        // UPDATE DIRECTION VECTOR
+    }
+
+    void HandleSquareBrickCollision(GameObject brick) {
+        // FIND A WAY TO DETECT SIDES AND MAKE CHANGES ACCORDINGLY.
+        // USE CROSS PRODUCT - FIND RIGHT DIAGONAL & LEFT DIAGONAL VECTOR
+        BoxCollider2D brick_bc2d = brick.GetComponent<BoxCollider2D>();
+        Vector2 extents = brick_bc2d.bounds.extents;
+        Vector2 center = brick_bc2d.bounds.center;
+        
+        Vector2 topRight = new Vector2(center.x + extents.x, center.y + extents.y);
+        Vector2 topLeft = new Vector2(center.x - extents.x, center.y + extents.y);
+        Vector2 bottomLeft = new Vector2(center.x - extents.x, center.y - extents.y);
+        Vector2 bottomRight = new Vector2(center.x + extents.x, center.y - extents.y);
+        bool isLeftOfRightDiagonal = IsLeft(topRight - bottomLeft, new Vector2(transform.position.x, transform.position.y) - bottomLeft);
+        bool isLeftOfLeftDiagonal = IsLeft(topLeft - bottomRight, new Vector2(transform.position.x, transform.position.y) - bottomRight);
+        Debug.Log("RIGHT DIAGONAL DIRECTION : " + (isLeftOfRightDiagonal ? "LEFT" : "RIGHT"));
+        Debug.Log("LEFT DIAGONAL DIRECTION : " + (isLeftOfLeftDiagonal ? "LEFT" : "RIGHT"));
+        if (isLeftOfLeftDiagonal && isLeftOfRightDiagonal) {
+            direction.x = -Mathf.Abs(direction.x);
+        } else if (isLeftOfLeftDiagonal && !isLeftOfRightDiagonal) {
+            direction.y = -Mathf.Abs(direction.y);
+        } else if (!isLeftOfLeftDiagonal && isLeftOfRightDiagonal) {
+            direction.y = Mathf.Abs(direction.y);
+        } else {
+            direction.x = Mathf.Abs(direction.x);
+        }
+        direction = direction.normalized;
+    }
+
+    bool IsLeft(Vector2 AB, Vector2 CB)
+    {
+        return (-AB.x * CB.y + AB.y * CB.x < 0);
+    }
+
+
+    IEnumerator BackToHolder() {
+        while (transform.position != BALL_HOLDER.position) {
+            direction = Vector2.zero;
+            transform.position = Vector3.MoveTowards(transform.position, BALL_HOLDER.position, speed * Time.fixedDeltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+        ResetBall();
+    }
+}
--- Assets/Scripts/SquareBrickController.cs
+++ Assets/Scripts/SquareBrickController.cs
@@ -25,6 +25,25 @@ public class SquareBrickController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hits == 0)
+            Destroy(gameObject);
+        brickText.text = hits.ToString();
+    }
+
+    IEnumerator DisplayColor() {
+        Color original = outlineSR.color;
+        Color temp = original;
+        temp.r = 0.9f;
+        temp.b = 0.9f;
+        outlineSR.color = temp;
+        yield return new WaitForSeconds(0.2f);
+        outlineSR.color = original;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Ball")) {
+            hits -= 1;
+            StartCoroutine(DisplayColor());
+        }
     }
 }
